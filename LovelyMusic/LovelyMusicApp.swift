e00fbb62abf0ca0acbd8a3398dba5c12e6e63584
Sakurathanh2003 <Thanhvt@fit-haui.edu.vn>
2023-07-06 10:54:07 +0700

    Initial Commit

---
+++ LovelyMusic/LovelyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovelyMusicApp.swift
+//  LovelyMusic
+//
+//  Created by Mei Mei on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LovelyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
